4e8fc88c1d9a95824a9e6366453c3e60d1879742
Padakandla298 <padakandlavenkatesh956@gmail.com>
2025-11-26 17:35:57 +0530

    first commit

--- src/test/java/Listeners/Dynamic_dropdown.java
+++ src/test/java/Listeners/Dynamic_dropdown.java
@@ -1,25 +1,41 @@
 package Listeners;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import java.util.*;
-public class Dynamic_dropdown {
-	public static WebDriver driver;
-	@Test
-	public void WindowHandles() {
-		driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		driver.findElement(By.xpath("//div[@class='cen-left-align']/fieldset/input")).sendKeys("ind");
-		  String text= driver.findElement(By.xpath("//ul[@id='ui-id-1']/li[1]/div")).getText();
-		  System.out.println(text);
-				//List<WebElement> options = driver.findElements(By.xpath("//ul[@id='ui-id-1']/li/div")).
-		
-		
-		
-	}
+import java.time.Duration;
+import java.util.List;
 
+public class Dynamic_dropdown {
+
+    public static WebDriver driver;
+
+    @Test
+    public void WindowHandles() {
+
+        driver = new ChromeDriver();
+
+        String str1 = "kiribati";
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        driver.findElement(By.xpath("//input[@id='autocomplete']")).sendKeys("kiri");
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='ui-id-1']/li")));
+
+              List<WebElement> list = driver.findElements(By.xpath("//ul[@id='ui-id-1']/li"));
+
+        for (WebElement L : list) {
+
+            String str = L.getText();
+            System.out.println(str);
+
+            if (str.equalsIgnoreCase(str1)) {
+                L.click();
+                break;
+            }
+        }
+    }
 }
